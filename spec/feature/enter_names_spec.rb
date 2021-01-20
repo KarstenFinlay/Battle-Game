1eae202a0a7b3fa7ad442582721abe7b88ccf0aa
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-20 16:10:58 +0000

    added helper file to dry up test code

--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Players_names' do
   scenario 'checks if form is filled in' do
-    visit('/')
-    fill_in :player_1_name, with: 'Karsten'
-    fill_in :player_2_name, with: 'Sean'
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Karsten vs Sean'
   end
 end
--- spec/feature/hit_points_spec.rb
+++ spec/feature/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Player hit points' do
   scenario 'Checks if player hit points are visible' do
-    visit('/')
-    fill_in :player_1_name, with: 'Karsten'
-    fill_in :player_2_name, with: 'Sean'
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Sean HP:100'
   end
 end
